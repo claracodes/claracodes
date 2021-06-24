9d44f94fcd86302231315ed7a3a97e9edf4f61f0
Clara Morgeneyer <clara_morgeneyer@web.de>
2021-06-24 14:47:04 -0700

    my own version

--- index.js
+++ index.js
@@ -18,33 +18,13 @@ let DATA = {
   }),
 };
 
-async function setWeatherInformation() {
-  await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
-  )
-    .then(r => r.json())
-    .then(r => {
-      DATA.city_temperature = Math.round(r.main.temp);
-      DATA.city_weather = r.weather[0].description;
-      DATA.city_weather_icon = r.weather[0].icon;
-      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZone: 'Europe/Stockholm',
-      });
-      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        timeZone: 'Europe/Stockholm',
-      });
-    });
-}
-
 async function setInstagramPosts() {
-  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('clara_morgen', 5);
   DATA.img1 = instagramImages[0];
   DATA.img2 = instagramImages[1];
   DATA.img3 = instagramImages[2];
+  DATA.img4 = instagramImages[3];
+  // DATA.img5 = instagramImages[4];
 }
 
 async function generateReadMe() {
@@ -56,11 +36,6 @@ async function generateReadMe() {
 }
 
 async function action() {
-  /**
-   * Fetch Weather
-   */
-  await setWeatherInformation();
-
   /**
    * Get pictures
    */
